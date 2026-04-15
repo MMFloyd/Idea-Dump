5a709dd6236d31d83cebf2a3030ed4afd580d04a
MMFloyd <michaelmurphyuk@proton.me>
2026-04-15 20:37:40 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'idea-dump-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
+});
